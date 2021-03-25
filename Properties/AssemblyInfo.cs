8af066c817b390c1c40e90f471c590bbde8c39c4
Omer Schloss <schlossomer@gmail.com>
2021-03-25 21:57:30 +0200

    #start new project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FlightInspector")]
+[assembly: AssemblyTitle("FlightInspection")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("FlightInspector")]
+[assembly: AssemblyProduct("FlightInspection")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
