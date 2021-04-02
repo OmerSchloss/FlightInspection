499d525ff17eb57e10c7e7a3f496860733ccd4f7
Omer Schloss <schlossomer@gmail.com>
2021-04-02 17:12:07 +0300

    CSVLoad window - fixed

--- CSVLoad.xaml.cs
+++ CSVLoad.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 
 namespace FlightInspection
 {
@@ -14,10 +16,18 @@ namespace FlightInspection
 
         private void Apply_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+                textbox_file_path.Text = openFileDialog.FileName; // text(string) location
         }
 
         private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
