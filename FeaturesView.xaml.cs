5e3d0fe54bed8efb1f09e0af32b2a9e3e03cf862
Amitai Yacobi <amitaiyacobi@gmail.com>
2021-04-06 08:26:38 +0300

    Added FeaturesMVVM

---
+++ FeaturesView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+
+namespace FlightInspection
+{
+    /// <summary>
+    /// Interaction logic for FeaturesView.xaml
+    /// </summary>
+    public partial class FeaturesView : UserControl
+    {
+        FeaturesViewModel featuresViewModel;
+
+        public FeaturesView(string csvPath, string xmlPath)
+        {
+            InitializeComponent();
+            featuresViewModel = new FeaturesViewModel(new FlightgearModel(csvPath, xmlPath, new TelnetClient()));
+            DataContext = featuresViewModel;
+
+            /* for (int i = 0; i < 10; ++i)
+             {
+                 ListBoxItem newItem = new ListBoxItem();
+                 newItem.Content = "Item " + i;
+                 listBox.Items.Add(newItem);
+             }*/
+        }
+    }
+}
---
+++ FeaturesViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FlightInspection
+{
+    class FeaturesViewModel : INotifyPropertyChanged
+    {
+        private FlightgearModel model;
+
+
+        public FeaturesViewModel(FlightgearModel model)
+        {
+            this.model = model;
+            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged("VM_" + e.PropertyName);
+            };
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public int VM_CurrentLineNumber
+        {
+            get { return model.CurrentLineNumber; }
+
+        }
+
+
+
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
